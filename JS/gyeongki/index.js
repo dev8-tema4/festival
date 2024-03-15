b22f4be9e454b0c73c20f25ffdde457d26502b97
dyddks <dyddks0115@naver.com>
2024-03-15 09:32:25 +0900

    경기도 페이드효과 추가

---
+++ JS/gyeongki/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const titleList = document.querySelectorAll('.title');
+  const contentList = document.querySelectorAll('.content');
+  const videoList = document.querySelectorAll('.video');
+
+  titleList.forEach((title) => {
+    
+  });
+});
