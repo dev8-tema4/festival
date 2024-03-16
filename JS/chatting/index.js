e6649ac5937af410f670b01536a8433d8f1f2bee
dyddks <dyddks0115@naver.com>
2024-03-16 12:42:55 +0900

    F/feat: 채팅창 열고닫기 메소드구현 및 메소드 파일 분리

--- JS/chatting/index.js
+++ JS/chatting/index.js
@@ -1,3 +1,4 @@
+// 메시지를 서버로 전송
 const sendChatMsg = function(){
     const message = document.querySelector('#messageInput').value;
     const packet = {
@@ -34,3 +35,50 @@ const recievePacketMessage = function ($parentSelector, message) {
     }
     parentElem.appendChild(childElem);
 }
+
+// 아이콘 클릭시 채팅창 생성
+const openChat = function(){
+  const body = document.querySelector('body');
+
+  const chatting = "<div class='chatting'>" +
+  "<div class='send'>" +
+  "<input type='text' id='messageInput' placeholder='메시지를 입력하세요'>" +
+  "<button id='btnChatMsg'>전송</button>" +
+  "</div>" +
+  "<div id='messages'>" +
+  "<!-- 메시지 출력 영역 -->" +
+  "</div>" +
+  "</div>";
+
+  body.insertAdjacentHTML('afterbegin', chatting);
+}
+
+// 채팅창 삭제
+const closeChat = function(){
+  const chatting = document.querySelector('.chatting');
+
+  chatting.remove();
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  let btnChatMsg = null;
+  const btnChatIcon = document.querySelector('.chatIcon');
+  let setIcon = false;
+
+  btnChatIcon.addEventListener('click', () => {
+    if(sessionStorage.getItem('id') === null){
+      alert('로그인이 필요한 서비스입니다.');         // 로그인이 되어있지 않으면 채팅창 이용불가
+      return;
+    }
+
+    if(setIcon === false){
+      openChat();
+      setIcon = true;
+      btnChatMsg = document.querySelector('.chatting');
+      btnChatMsg.addEventListener('click', sendChatMsg);
+    } else {
+      closeChat();
+      setIcon = false;
+    }
+  });
+});
