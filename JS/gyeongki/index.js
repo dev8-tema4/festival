a476e25a89a47e9c170d4c7dcde663b4e0e10741
dyddks <dyddks0115@naver.com>
2024-03-16 18:59:27 +0900

    F/feat: 경기도 content 더보기 클릭메소드 추가

--- JS/gyeongki/index.js
+++ JS/gyeongki/index.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const festival = document.querySelector('.fastival');
   const titleList = document.querySelectorAll('.title');
   const contentList = document.querySelectorAll('.content');
   const videoList = document.querySelectorAll('.video');
-
-  titleList.forEach((title) => {
+  contentList.forEach((element) => {
+    let plus = element.nextElementSibling;
     
+    plus.addEventListener('click', () => {
+      plus.remove();
+      element.style.overflow = 'visible';
+    });
   });
 });
